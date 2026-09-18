dc2de1ddb7112ce133c5f42a04da99ec73be4bc6
HypatiaG <ikhsannul201@gmail.com>
2026-09-18 10:03:06 +0700

    feat: update CtaSection to use Button component from heroui and implement routing

--- src/components/customs/landings/CtaSection.tsx
+++ src/components/customs/landings/CtaSection.tsx
@@ -1,6 +1,9 @@
 'use client';
+import { Button } from '@heroui/react';
+import { useRouter } from 'next/navigation';
 
 export default function CtaSection() {
+  const router = useRouter();
   return (
     <section className="px-32 py-14 md:py-20 flex justify-center bg-white">
       <div className="w-full rounded-[2rem] bg-blue-600 px-6 py-12 text-center text-white md:px-12 md:py-16 shadow-lg">
@@ -15,10 +18,13 @@ export default function CtaSection() {
           Proses lebih mudah, lebih cepat, dan terukur secara digital. Semua dalam satu platform.
         </p>
 
-        <button className="mt-8 rounded-lg bg-yellow-400 px-8 py-3 text-sm font-bold text-white transition hover:bg-yellow-500 md:mt-10 md:px-10 md:text-base">
+        <Button
+          variant='primary'
+          onPress={() => router.push('/e-cerapan')}
+          className="mt-8 rounded-lg bg-yellow-400 px-8 py-3 text-sm font-bold text-white transition hover:bg-yellow-500 md:mt-10 md:px-10 md:text-base"
+        >
           Coba Sekarang
-        </button>
-
+        </Button>
       </div>
     </section>
   );
